c37857fb2aaf3a131a8aecf00b1364d83dcc0f10
Monsalves <f.monsalves03@ufromail.cl>
2023-07-05 00:18:25 -0400

    ventana crearPublicacion

--- src/main/java/GUI/VendedorVents/LogueadoVendedorVentana.java
+++ src/main/java/GUI/VendedorVents/LogueadoVendedorVentana.java
@@ -1,8 +1,8 @@
 package GUI.VendedorVents;
 
-import GUI.CrearPublicacionVentana;
 import GUI.LoginGUI;
 import GUI.VerMisPublicVentana;
+import GUI.crearPublicacionGUI;
 import Usuarios.Usuario;
 import Usuarios.Vendedor;
 
@@ -57,7 +57,7 @@ public class LogueadoVendedorVentana extends JFrame implements ActionListener {
             setVisible(false);
 
         }else if (e.getSource() == crearPublicacionButton){
-            CrearPublicacionVentana crearPublicacionVentana = new CrearPublicacionVentana(usuarios, vendedor);
+            crearPublicacionGUI crearPublicacionVentana = new crearPublicacionGUI(usuarios, vendedor);
             crearPublicacionVentana.Pantalla();
             setVisible(false);
         }else if (e.getSource() == verMisPublicacionesButton){
