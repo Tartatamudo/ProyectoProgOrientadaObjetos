c7be09649218f8fa8845572f26e073e549b74144
Monsalves <f.monsalves03@ufromail.cl>
2023-07-07 00:53:46 -0400

    ver Mis publicaciones

---
+++ src/main/java/GUI/CompradorVents/MostrarServiciosVendedoresGUI.java
@@ -0,0 +1,16 @@
+package GUI.CompradorVents;
+
+import javax.swing.*;
+
+public class MostrarServiciosVendedoresGUI {
+    private JPanel ventanaPNL;
+    private JPanel inferiorPNL;
+    private JPanel superiorPNL;
+    private JLabel iconLBL;
+    private JComboBox comboBox1;
+    private JButton button1;
+    private JList list1;
+    private JButton button2;
+    private JComboBox comboBox2;
+    private JButton button3;
+}
